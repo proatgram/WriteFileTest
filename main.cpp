84a1f221dc542ec2c747e1b4727bd6ff73929ad1
TheTimBrick <tgh6407@gmail.com>
2021-03-03 00:37:56 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+/*
+int main() {
+  int data;
+  std::ofstream newFileWrite;
+  newFileWrite.open("inventory.dat", std::ios::app);
+  newFileWrite << 0xA0 << ",";
+  newFileWrite.close();
+  std::ifstream newFileRead;
+  newFileRead.open("inventory.dat");
+  newFileRead >> data;
+  std::printf("%x\n", data);
+  switch (data*) {
+    case ",":
+      break;
+  }
+  newFileRead.close();
+}
+*/
+
+class file {
+public: 
+
+  int writeString(const char * fileName, const std::string& string, const int sizeOfBuffer) {
+
+    void* buffer = static_cast<void*>(&string);
+
+    FILE *file = std::fopen(fileName, "w");
+
+    if(!file) {
+      return EXIT_FAILURE;
+    }
+    std::fwrite(buffer, sizeof(char), sizeof(string), file);
+  };
+
+private:  
+
+
+};
+
+int main() {
+  file newFile;
+  newFile.writeString("example.dat", "Hello!",3);
+
+
+  /*
+  FILE *file = std::fopen("inventory.dat", "rb+");
+  if (!file) {
+    //std::pererr("file opening failed");
+    return EXIT_FAILURE;
+  }
+  */
+  return 0;
+}
