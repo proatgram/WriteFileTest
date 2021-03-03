93b3e6808c234af25b8056798d12a9ea273f1dcd
TheTimBrick <tgh6407@gmail.com>
2021-03-03 05:05:03 +0000

    i fuciking hate this

--- main.cpp
+++ main.cpp
@@ -28,14 +28,30 @@ public:
 
     const void* buffer = reinterpret_cast<const void*>(string.c_str());
 
-    FILE *file = std::fopen(fileName, "w");
+    FILE *file = std::fopen(fileName, "w+");
 
     if(!file) {
       return EXIT_FAILURE;
     }
     std::fwrite(buffer, sizeof(char),string.size() , file);
-    fclose(buffer);
-    return EXIT_SUCCESS
+    std::fclose(file);
+    return EXIT_SUCCESS;
+  };
+  int writeRaw(const char * fileName, const int rawArray[]) {
+    
+    FILE *file = std::fopen(fileName, "wb+");
+
+    for (int times = 0; times != 2; times++) {
+      int data = rawArray[times];
+      printf("%d",data);
+      void* buffer = (void*)data;
+      //const void* buffer = reinterpret_cast<const void*>(data);
+      std::fwrite(buffer, sizeof(int), sizeof(rawArray), file);
+    }
+
+    //std::fwrite(buffer, sizeof(int), 2, file);
+    //std::fclose(file);
+    return EXIT_SUCCESS;
   };
 
 private:  
@@ -45,8 +61,9 @@ private:
 
 int main() {
   file newFile;
-  newFile.writeString("example.dat", "Hello!");
-  
+  newFile.writeString("example.dat", "Hello World");
+  int testArray[] = {0x01,0x02};
+  newFile.writeRaw("example.dat",testArray);
 
   /*
   FILE *file = std::fopen("inventory.dat", "rb+");
@@ -55,5 +72,4 @@ int main() {
     return EXIT_FAILURE;
   }
   */
-  return 0;
 }
