c85c86e6d4672a5e978fd748ff478a0c1eb9ec49
TheTimBrick <tgh6407@gmail.com>
2021-03-04 23:53:35 +0000

    Added readRaw method, error detection works, gets segmentation error if error detection is disabled.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <cstdio>
-#include <fstream>
-#include <cstdlib>
 #include <string>
 #include <cstring>
 #include <cstdint>
+#include <vector>
+
 
 /*
 int main() {
@@ -47,16 +47,38 @@ public:
         if (!file) {
             return EXIT_FAILURE;
         }
-
         for (uint32_t i = 0; i < arraySize; i++)
-            printf("%02X ", rawArray[i]);
+            printf("0x%x \n", rawArray[i]);
 
         fwrite(reinterpret_cast<const void*>(rawArray), 1, arraySize, file);
 
         fclose(file);
         return EXIT_SUCCESS;
     };
-
+    std::vector<uint8_t>* readRaw(const char* fileName, const uint32_t numBytes) {
+      FILE* file = std::fopen(fileName, "rb");
+      std::vector<uint8_t> *contents = new std::vector<uint8_t>();
+      
+      if (!file) {
+        contents->push_back(0x01);
+        return contents;
+      }
+      
+      int byte;
+      while ((byte = std::getc(file)) != EOF ) {
+        contents->push_back(byte);
+      }
+      if (ferror(file)) {
+        contents->push_back(0x02);
+        return contents;
+      }
+      else if (feof(file)) {
+        contents->push_back(0x03);
+        return contents;
+      }
+      std::fclose(file);
+      return contents;
+    }
 private:
 };
 
@@ -65,7 +87,13 @@ int main() {
     newFile.writeString("example.dat", "Hello World");
     const uint8_t testArray[] = { 0x01, 0x02, 0x03 };
     newFile.writeRaw("example1.dat", testArray, sizeof(testArray));
-
+    const uint8_t testArrayTwo[] = { 0x04, 0x05, 0x06 };
+    newFile.writeRaw("example1.dat", testArrayTwo, sizeof(testArrayTwo));
+    std::vector<uint8_t>* vector = newFile.readRaw("example1", 5);
+    printf("%x\n",vector);
+    for (int times = 0x00; times != vector->size(); times++){
+      printf("0x%x\n",vector->at(times));
+    }
     /*
     FILE *file = std::fopen("inventory.dat", "rb+");
     if (!file) {
