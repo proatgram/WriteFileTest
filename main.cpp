637a95d240d68ff75c7db5c89cd5539457258626
TheTimBrick <34280824+proatgram@users.noreply.github.com>
2021-03-03 21:07:53 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <cstdlib>
 #include <string>
 #include <cstring>
+#include <cstdint>
+
 /*
 int main() {
   int data;
@@ -23,57 +25,52 @@ int main() {
 */
 
 class file {
-public: 
+public:
 
-  int writeString(const char * fileName, const std::string& string) {
+    int writeString(const char* fileName, const std::string& string) {
 
-    const void* buffer = reinterpret_cast<const void*>(string.c_str());
+        const void* buffer = reinterpret_cast<const void*>(string.c_str());
 
-    FILE *file = std::fopen(fileName, "w");
+        FILE* file = std::fopen(fileName, "w");
 
-    if(!file) {
-      return EXIT_FAILURE;
-    }
-    std::fwrite(buffer, sizeof(char),string.size() , file);
-    std::fclose(file);
-    return EXIT_SUCCESS;
-  };
-  int writeRaw(const char * fileName,const int (&rawArray)[]) {
-    
-    FILE *file = std::fopen(fileName, "wb");
-    if(!file) {
-      return EXIT_FAILURE;
-    }
-    for (int times = 0; times != 2; times++) {
-      void* buffer = reinterpret_cast<void*>(rawArray[times]);
-      
-      printf("0x%x\n",buffer);
-      //void* buffer = (void*)data;
-      std::fwrite(buffer, 1, 1, file);
-    }
+        if (!file) {
+            return EXIT_FAILURE;
+        }
+        std::fwrite(buffer, sizeof(char), string.size(), file);
+        std::fclose(file);
+        return EXIT_SUCCESS;
+    };
 
-    //std::fwrite(buffer, sizeof(int), 2, file);
-    //std::fclose(file);
-    return EXIT_SUCCESS;
-  };
+    int writeRaw(const char* fileName, const uint8_t* rawArray, const uint32_t arraySize) {
 
-private:  
+        FILE* file = std::fopen(fileName, "wb");
+        if (!file) {
+            return EXIT_FAILURE;
+        }
 
+        for (uint32_t i = 0; i < arraySize; i++)
+            printf("%02X ", rawArray[i]);
 
+        fwrite(reinterpret_cast<const void*>(rawArray), 1, arraySize, file);
+
+        fclose(file);
+        return EXIT_SUCCESS;
+    };
+
+private:
 };
 
 int main() {
-  file newFile;
-  newFile.writeString("example.dat", "Hello World");
-  const int testArray[] = {0x01, 0x02, 0x03};
-  const int (&newArrayRef)[] = testArray[];
-  newFile.writeRaw("example1.dat",newArrayRef);
+    file newFile;
+    newFile.writeString("example.dat", "Hello World");
+    const uint8_t testArray[] = { 0x01, 0x02, 0x03 };
+    newFile.writeRaw("example1.dat", testArray, sizeof(testArray));
 
-  /*
-  FILE *file = std::fopen("inventory.dat", "rb+");
-  if (!file) {
-    //std::pererr("file opening failed");
-    return EXIT_FAILURE;
-  }
-  */
+    /*
+    FILE *file = std::fopen("inventory.dat", "rb+");
+    if (!file) {
+      //std::pererr("file opening failed");
+      return EXIT_FAILURE;
+    }
+    */
 }
