ac7219334643bd2e53c334cd24b7a0b4556ca8d1
TheTimBrick <tgh6407@gmail.com>
2021-03-05 22:17:17 +0000

    Optimisation.

--- main.cpp
+++ main.cpp
@@ -11,46 +11,46 @@ public:
 
     int writeString(const char* fileName, const std::string& string) {
 
-        const void* buffer = reinterpret_cast<const void*>(string.c_str());
+      const void* buffer = reinterpret_cast<const void*>(string.c_str());
 
-        FILE* file = std::fopen(fileName, "w");
+      FILE* file = std::fopen(fileName, "w");
 
-        if (!file) {
-            return EXIT_FAILURE;
-        }
-        std::fwrite(buffer, sizeof(char), string.size(), file);
-        std::fclose(file);
-        return EXIT_SUCCESS;
+      if (!file) {
+        return EXIT_FAILURE;
+      }
+      std::fwrite(buffer, sizeof(char), string.size(), file);
+      std::fclose(file);
+      return EXIT_SUCCESS;
     };
+
     std::string readString(const char* fileName) {
       FILE* file = std::fopen(fileName, "r");
       if (!file) {
-        return EXIT_FAILURE;
+      std::string EXIT_FAIL = std::to_string(EXIT_FAILURE);
+      return EXIT_FAIL;
       }
       
     }
     int writeRaw(const char* fileName, const uint8_t* rawArray, const uint32_t arraySize) {
 
-        FILE* file = std::fopen(fileName, "wb");
-        if (!file) {
-            return EXIT_FAILURE;
-        }
+      FILE* file = std::fopen(fileName, "wb");
+      if (!file) {
+        return EXIT_FAILURE;
+      }
         
-        
+      fwrite(reinterpret_cast<const void*>(rawArray), 1, arraySize, file);
 
-        fwrite(reinterpret_cast<const void*>(rawArray), 1, arraySize, file);
-
-        fclose(file);
-        return EXIT_SUCCESS;
+      std::fclose(file);
+      return EXIT_SUCCESS;
     };
+
     std::vector<uint8_t>* readRaw(const char* fileName, const uint32_t numBytes) {
       FILE* file = std::fopen(fileName, "rb");
       std::vector<uint8_t> *contents = new std::vector<uint8_t>();
       
       if (!file) {
-        contents->push_back(0x01);
-        printf("Error opening the file errno: (%d: %s)\n", 
-          errno, strerror(errno));
+        contents->push_back(EXIT_FAILURE);
+        contents->push_back(errno);
         return contents;
       }
       
@@ -59,11 +59,13 @@ public:
         contents->push_back(byte);
       }
       if (ferror(file)) {
-        contents->push_back(0x02);
+        contents->push_back(EXIT_FAILURE);
+        contents->push_back(errno);
         return contents;
       }
       else if (feof(file)) {
-        contents->push_back(0x03);
+        contents->push_back(EOF);
+        std::fclose(file);
         return contents;
       }
       std::fclose(file);
@@ -73,19 +75,24 @@ private:
 };
 
 int main() {
-    rrFile newFile;
-    newFile.writeString("example.dat", "Hello World");
-    const uint8_t testArray[] = { 0x01, 0x02, 0x03 };
-    newFile.writeRaw("example1.dat", testArray, sizeof(testArray));
-    std::vector<uint8_t>* vector = newFile.readRaw("example1.dat", 5);
-    for (int times = 0x00; times != vector->size(); times++){
-      printf("0x%x\n",(vector->at(times)));
-    }
-    /*
-    FILE *file = std::fopen("inventory.dat", "rb+");
-    if (!file) {
-      //std::pererr("file opening failed");
-      return EXIT_FAILURE;
-    }
-    */
+  rrFile newFile;
+
+  newFile.writeString("example.dat", "Hello World");
+
+  const uint8_t testArray[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F };
+
+  newFile.writeRaw("example1.dat", testArray, sizeof(testArray));
+
+  std::vector<uint8_t>* vector = newFile.readRaw("example1.dat", 5);
+
+  for (int times = 0x00; times != vector->size(); times++){
+
+
+    printf("0x%x\n",(vector->at(times)));
+
+
+  }
+
+  return 0;
+  
 }
